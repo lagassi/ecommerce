241dcdae1ce4ac5f877236782aa6a35cf415f7fc
lagassi <wilhan_cross@hotmail.com>
2017-10-30 12:50:58 -0200

    Tela de login

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -9,6 +9,8 @@ Class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 
@@ -28,7 +30,7 @@ Class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 	}
 
 	private function setData($data = array())
@@ -49,7 +51,7 @@ Class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 	}
