b6c2e4031cd08888dc785a0c4be1ed29208402e6
lagassi <wilhan_cross@hotmail.com>
2017-12-08 08:31:55 -0300

    Erro para add imagem no momento que add produto

--- views-cache/products-create.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/products-create.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -55,10 +55,10 @@
             </div>
             <div class="form-group">
               <label for="file">Foto</label>
-              <input type="file" class="form-control" id="file" name="file" value="">
+              <input type="file" class="form-control" id="file" name="file" value="<?php echo htmlspecialchars( $product["vlweight"], ENT_COMPAT, 'UTF-8', FALSE ); ?>">
               <div class="box box-widget">
                 <div class="box-body">
-                  <img class="img-responsive" id="image-preview" src="" alt="Photo">
+                  <img class="img-responsive" id="image-preview" src="<?php echo htmlspecialchars( $product["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="Photo">
                 </div>
               </div>
             </div>
